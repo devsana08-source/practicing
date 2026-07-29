3fb7c79155debf9c711a7551dc2c61f0856556a0
Dev Sana <devsana08@gmail.com>
2026-07-29 22:16:30 +0530

    Lab 2.2.8 – Finding the Value of π (Leibniz Formula)

---
+++ pi_leibniz.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	double pi4 = 0.;
+	long   n;
+
+	cout << "Number of iterations? ";
+	cin >> n;
+
+	for(long i = 0;i < n;i++) 
+	
+	{ 
+	    if(i % 2 ==0) 
+	    { 
+	        pi4 = pi4 + (1.0/(2 * i + 1));
+	        
+	    }else 
+	    { 
+	        pi4 = pi4 - (1.0/(2 * i + 1)); 
+	        
+	    } 
+	    
+	}
+
+	cout.precision(20);
+	cout << "Pi = " << (pi4 * 4.) << endl;
+	return 0;
+}
